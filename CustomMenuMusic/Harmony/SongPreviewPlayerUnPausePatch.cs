2cb53b9b5ec80f0af8ef2eca8fc2010120382bc3
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-03-22 23:46:59 +0900

    Unpauseにパッチ当てるの忘れてた

---
+++ CustomMenuMusic/Harmony/SongPreviewPlayerUnPausePatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomMenuMusic.Harmony
+{
+    [HarmonyPatch(typeof(SongPreviewPlayer), nameof(SongPreviewPlayer.UnPauseCurrentChannel))]
+    public class SongPreviewPlayerUnPausePatch
+    {
+        public static void Postfix() => CustomMenuMusic.IsPauseOrFadeOut = false;
+    }
+}
