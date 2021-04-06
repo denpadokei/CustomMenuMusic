52096a73cccfb4ff6be67d7a14a94087bdb219e6
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-04-07 02:08:42 +0900

    スキップボタン実装 ナウプレイキャンバスをまっすぐにした

--- CustomMenuMusic/Harmony/SongPreviewPlayerPauseAndSongPreviewPlayerFadeOutPatchPatch.cs
+++ CustomMenuMusic/Harmony/SongPreviewPlayerPauseAndSongPreviewPlayerFadeOutPatchPatch.cs
@@ -6,12 +6,20 @@ namespace CustomMenuMusic.Harmony
     [HarmonyPatch(typeof(SongPreviewPlayer), nameof(SongPreviewPlayer.PauseCurrentChannel))]
     public class SongPreviewPlayerPausePatch
     {
-        public static void Postfix() => CustomMenuMusic.IsPauseOrFadeOut = true;
+        public static void Postfix()
+        {
+            CustomMenuMusic.IsPauseOrFadeOut = true;
+            CustomMenuMusic.IsMenuSongPlaying = false;
+        }
     }
 
     [HarmonyPatch(typeof(SongPreviewPlayer), nameof(SongPreviewPlayer.FadeOut), new Type[] { typeof(float) })]
     public class SongPreviewPlayerFadeOutPatch
     {
-        public static void Postfix() => CustomMenuMusic.IsPauseOrFadeOut = true;
+        public static void Postfix()
+        {
+            CustomMenuMusic.IsPauseOrFadeOut = true;
+            CustomMenuMusic.IsMenuSongPlaying = false;
+        }
     }
 }
